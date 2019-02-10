ba3021007d3cad6a2c26caeb81edff2ddcfad75a
Daniela Rosca <danielarosca18@gmail.com>
2019-02-10 11:09:14 +0200

    added info

---
+++ Java Basics/src/MyFirstNumber.java
@@ -0,0 +1,14 @@
+
+public class MyFirstNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 5;
+			int b = 9;
+			int c = 11;
+			int total = a + b + c;
+			int total2 = 1000 - total;
+			System.out.println(total2);
+	}
+
+}
